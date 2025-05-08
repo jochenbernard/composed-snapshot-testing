7cf649bebf0c9048ab133459fa813b40cc9a2cdf
Jochen Bernard <jochenbernard@gmail.com>
2025-05-08 18:37:54 +0200

    Add test snapshots

--- Tests/ComposedSnapshotTestingTests/ComposedSnapshotTestingTests.swift
+++ Tests/ComposedSnapshotTestingTests/ComposedSnapshotTestingTests.swift
@@ -1,6 +1,39 @@
+import SwiftUI
 import Testing
+import SnapshotTesting
 @testable import ComposedSnapshotTesting
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite(.snapshots(record: .failed))
+struct ComposedSnapshotTestingTests {
+    @MainActor @Test
+    @available(iOS 15.0, *)
+    func testSnapshotPhone() {
+        assertSnapshot(
+            of: TestView(),
+            as: .image(on: .iPhoneSe)
+        )
+    }
+
+    @MainActor @Test
+    @available(iOS 15.0, *)
+    func testSnapshotPad() {
+        assertSnapshot(
+            of: TestView(),
+            as: .image(on: .iPadPro12_9)
+        )
+    }
+
+    @available(iOS 15.0, *)
+    struct TestView: View {
+        var body: some View {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
 }
