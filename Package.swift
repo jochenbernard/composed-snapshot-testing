3eb92e30cbee8665968f86d0be725d6f22456b33
Jochen Bernard <jochenbernard@gmail.com>
2025-05-08 17:50:18 +0200

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,20 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "ComposedSnapshotTesting",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ComposedSnapshotTesting",
-            targets: ["ComposedSnapshotTesting"]),
+            targets: ["ComposedSnapshotTesting"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ComposedSnapshotTesting"),
+        .target(name: "ComposedSnapshotTesting"),
         .testTarget(
             name: "ComposedSnapshotTestingTests",
             dependencies: ["ComposedSnapshotTesting"]
-        ),
+        )
     ]
 )
