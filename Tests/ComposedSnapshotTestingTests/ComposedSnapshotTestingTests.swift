1257299658b8f4796a2cfb7b4a775c08ed2177d1
Jochen Bernard <jochenbernard@gmail.com>
2025-05-08 17:48:11 +0200

    Initial Commit

---
+++ Tests/ComposedSnapshotTestingTests/ComposedSnapshotTestingTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import ComposedSnapshotTesting
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
